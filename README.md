4eceba7f672fd884b281c0a9806e54a7c29569f1
yashodgayashan <yashodgayashan@gmail.com>
2022-11-29 14:15:17 +0530

    Created action

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # choreo-code-push-trigger
+
+This action will trigger the code push api to trigger the auto deploy
+
+## Example
+
+```yaml
+steps:
+  - name: Trigger Code Push
+    uses: choreo-templates/choreo-code-push-trigger@v1.0.0
+    with:
+      baseURL: ${{ BASE_URL }}
+      componentId: ${{ COMPONENT_ID }}
+      versionId: ${{ VERSION_ID }}
+      secret: ${{ SECRET }}
+      payload: ${{ PAYLOAD }}
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core');
+const axios = require('axios').default;
+
+try {
+  const baseURL = core.getInput('baseURL');
+  const componentId = core.getInput('componentId');
+  const versionId = core.getInput('versionId');
+  const secret = core.getInput('secret');
+  const payload = core.getInput('payload');
+
+  const url = `${baseURL}/choreo/component/code-push?componentId=${componentId}&versionId=${versionId}`;
+  const reqPayload = JSON.parse(payload);
+  const config = {
+    headers: {
+      "X-GitHub-Event": "push",
+      "X-Hub-Signature-256": secret,
+      "Action": "auto-deploy"
+    }
+  };
+
+  console.log("Payload : ", payload);
+  axios.post(url, reqPayload, config).then(
+    () => {
+      core.setOutput("choreo-code-push-trigger", "saved");
+      console.log("choreo-code-push-trigger", "saved");
+    }
+  ).catch((error => {
+        console.error('Error', error);
+        if (error.response) {
+            core.setOutput("choreo-status", error.response.data);
+            console.log(error.response.status);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log('Error', error.message);
+            core.setOutput("choreo-status", "failed");
+        }
+    })
+  )   
+} catch (e) {
+    core.setOutput("choreo-code-push-trigger", "failed");
+    console.log("choreo-code-push-trigger", "failed");
+    console.log("choreo-code-push-trigger", e.message);
+}
